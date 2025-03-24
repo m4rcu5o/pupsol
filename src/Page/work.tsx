28878ac8dd0f3a6a157a34b14216bd3b9ba8affb
hudesdev <hudes0112@gmail.com>
2025-03-24 17:09:51 +0900

    update: link icon

--- src/Page/work.tsx
+++ src/Page/work.tsx
@@ -1,12 +1,13 @@
 import { projects } from "../config/menu"
 import { Link } from "react-router-dom"
+import { FaLink } from "react-icons/fa";
 
 export const Work = () => {
     return <div className='py-8 text-white grid grid-cols-1 md:grid-cols-2 gap-4'>
         {projects.map((val, index) =>
             <div key={index} className="project-block relative border border-dashed border-zinc-400 px-4 py-4 bg-[#0d1d31] flex flex-col justify-between rounded-lg">
-                <Link to={val.links[0].link} className="text-left text-white hover:underline text-md" target="block">
-                    {val.title}
+                <Link to={val.links[0].link} className="text-left text-white text-md" target="block">
+                    <div className="flex items-center hover:underline"><FaLink className="inline-block mr-2 text-sm" /> {val.title}</div> 
                 </Link>
                 <img src={val.image} alt="" className="mt-3 w-full object-cover rounded-md" />
                 <div className="w-full mt-3">

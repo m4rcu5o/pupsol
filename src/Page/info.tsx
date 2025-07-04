ac471f79561c72972e4608be9e6cd567fde85509
m4rcu5o <hudes0112@gmail.com>
2025-07-04 09:46:40 +0000

    update: bio

--- src/Page/info.tsx
+++ src/Page/info.tsx
@@ -4,7 +4,7 @@ import { Quote } from '../Component/Quote';
 
 export const About = () => {
     return <div className='py-8'>
-        <div className="text-zinc-400 text-md leading-6">A passionate Blockchain Full-stack with expertise in Web3 and responsive web applications. Focusing on <span className='text-white'> Smart contracts, trading dApps and Blockchain AI Agent</span> on<span className='text-white'> Solana, EVM </span>based blockchain. Experience spans NFTs, crypto games, DeFi, DEXs and crypto trading bots. As a Full-Stack Engineer, I builds scalable applications using <span className='text-white'> React, Next.js, TypeScript, Node.js, Rust, Golang, and Solidity</span>. Passionate about blockchain innovation, he is always exploring new opportunities for growth.
+        <div className="text-zinc-400 text-md leading-6">A passionate Blockchain Full-stack with expertise in Web3 and responsive web applications. Focusing on <span className='text-white'> Smart contracts, trading dApps and Blockchain AI Agent</span> on<span className='text-white'> Solana, EVM </span>compatible blockchain. Experience spans NFTs, crypto games, DeFi, DEXs and crypto trading bots. As a Full-Stack Engineer, I builds scalable applications using <span className='text-white'> React, Next.js, TypeScript, Node.js, Rust, Golang, and Solidity</span>. Passionate about blockchain innovation, he is always exploring new opportunities for growth.
             {/* Visit for more detail <a href='https://drive.google.com/file/d/1dPYF77CKMM1y-00nzpBjBdN8MfWcJMdg/view?usp=drive_link' target='block' className='underline hover:text-white transition text-white'>here</a> */}
         </div>
         {/* Reference */}
@@ -13,7 +13,7 @@ export const About = () => {
                 reference.map((v, i) =>
                     <Quote clinet={v.name} url={v.url} key={"quote" + i} tweet={v.tweetUrl}>{v.content}</Quote>
                 )
-            }
+            }f
         </div>
         <div className='w-full overflow-auto'>
 
